c2e69447ed556117efa48847d118ddf1353a8ccb
Padraig20 <styllpatrick@gmail.com>
2025-12-03 10:30:43 +0100

    feat: update gene tss locator script to take multiple ensembl ids in a single run

--- data_prep/get_gene_tss.sh
+++ data_prep/get_gene_tss.sh
@@ -1,27 +1,35 @@
-# Usage: ./get_gene_tss.sh <input.gtf> <ENSEMBL_GENE_ID>
+#!/usr/bin/env bash
 
-if [[ $# -ne 2 ]]; then
-    echo "Usage: $0 <input.gtf> <ENSEMBL_GENE_ID>"
+if [[ $# -lt 2 ]]; then
+    echo "Usage: $0 <input.gtf> <ENSEMBL_GENE_ID_1> [ENSEMBL_GENE_ID_2 ... ENSEMBL_GENE_ID_N]"
     exit 1
 fi
 
 INPUT_FILE="$1"
-GENE_ID_SEARCH="$2"
-WINDOW=98304 # 196,608 kb window centered on TSS, as in Enformer
+shift                     # removes GTF file from args
+GENE_IDS=("$@")           # all remaining args are gene IDs
 
-awk -F'\t' -v gene_search="$GENE_ID_SEARCH" -v window="$WINDOW" '
+WINDOW=98304              # 196,608 kb window centered on TSS, as in Enformer
+
+# convert bash array into AWK regex => id1|id2|id3
+GENE_REGEX=$(printf "%s|" "${GENE_IDS[@]}")
+GENE_REGEX=${GENE_REGEX%|}   # remove trailing |
+
+awk -F'\t' -v genes="$GENE_REGEX" -v window="$WINDOW" '
+BEGIN {
+    pattern = "(" genes ")"
+}
 $3=="gene" {
     OFS = "\t"
 
+    # TSS depends on strand... start or end
     if ($7 == "+") tss = $4
     else           tss = $5
 
     gene_id = "NA"
-    if (match($9, /gene_id "([^"]+)"/, a)) {
-        gene_id = a[1]
-    }
+    if (match($9, /gene_id "([^"]+)"/, a)) gene_id = a[1]
 
-    if (gene_id == gene_search) {
+    if (gene_id ~ pattern) {
         print gene_id, tss-window, tss+window
     }
 }' "$INPUT_FILE"
