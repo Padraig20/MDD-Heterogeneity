fix: stack mu and sigma for loss computation in evaluation script

training/models/mlp_deep_ensemble.py
@@ -51,8 +51,9 @@ class MLPEnsemble(nn.Module):
             vars  = []
             for model in self.models:
                 model.train()
-                means.append(model(x)[..., :self.output_dim])
-                vars.append(model(x)[..., self.output_dim:].clamp(min=1e-8)) # avoid zero div
+                preds = model(x)
+                means.append(preds[..., :self.output_dim])
+                vars.append(preds[..., self.output_dim:].clamp(min=1e-8)) # avoid zero div
             return means, vars
 
         else: # calculate aggregated predictions and uncertainties
@@ -62,7 +63,7 @@ class MLPEnsemble(nn.Module):
                 model.eval()
                 preds = model(x) # shape (batch_size, output_dim*2)
                 mu    = preds[..., :self.output_dim]
-                var   = preds[..., self.output_dim:]
+                var   = preds[..., self.output_dim:].clamp(min=1e-8) # avoid zero div
                 means.append(mu)
                 vars.append(var)
 

training/utils.py
@@ -329,11 +329,12 @@ def evaluate_ensemble_model(
             inputs, targets = batch
             inputs, targets = inputs.to(device), targets.to(device)
 
-            outputs, aleatoric_unc, epistemic_unc = model(inputs)
+            prediction, aleatoric_unc, epistemic_unc = model(inputs)
             
             aleatoric_uncertainties.append(aleatoric_unc.mean(dim=0)) # avg aleatoric uncertainty across cells
             epistemic_uncertainties.append(epistemic_unc.mean(dim=0)) # avg epistemic uncertainty across cells
 
+            outputs = torch.stack([prediction, aleatoric_unc], dim=2) # shape (batch_size, output_dim, 2)
             composite_loss = torch.tensor(0.0, device=device)
             for key in loss_dict.keys():
                 loss = loss_dict[key](outputs, targets) * loss_lambda_dict[key]
@@ -341,8 +342,8 @@ def evaluate_ensemble_model(
                 composite_loss += loss
 
             log_dict[f"{mode}/loss"] += composite_loss.item()
-            metric_cells.update(outputs, targets)
-            metric_genes.update(outputs, targets)
+            metric_cells.update(prediction, targets)
+            metric_genes.update(prediction, targets)
 
     log_dict[f"{mode}/loss"] /= len(eval_loader)
     log_dict[f"{mode}/pearson_cells"] = metric_cells.compute().mean().item()